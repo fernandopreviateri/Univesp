d73b15b7087c8ba505711be99642857702fb6163
Erika Vaz <2002809@aluno.univesp.br>
2021-11-01 18:48:46 -0300

    atualização do forms.py, models.py e cadastro2.html

--- app/forms.py
+++ app/forms.py
@@ -5,45 +5,34 @@ from django import forms
 class MyCommentForm(forms.ModelForm):
     class Meta(object):
         model = Paciente
-        fields = ['name', 'email', 'message', 'address', 'responsavel','idade','phone']
+        fields = ['prontuario', 'name', 'dn', 'cpf', 'message']
         widgets = {
-    'name': forms.TextInput(
-    attrs={
-     'class': 'form-control'
-     }
-    ),
-    'responsavel': forms.TextInput(
-    attrs={
-     'class': 'form-control'
-     }
-    ),
-    'idade': forms.DateTimeInput(
-    attrs={
-     'class': 'form-control',
-     'type': 'date'
-    }
-    ),
-    'address': forms.TextInput(
-    attrs={
-     'class': 'form-control'
-     }
-    ),
-    'phone': forms.TextInput(
-    attrs={
-     'class': 'form-control',
-     'placeholder': '(99) 99999-9999',
-     'id': 'telefone',
-     'name': 'telefone'
-     }
-    ),
-   'email': forms.TextInput(
-    attrs={
-     'class': 'form-control'
-     }
-    ),
-    'message': forms.Textarea(
-    attrs={
-     'class': 'form-control'
-     }
-    ),
+            'prontuario': forms.TextInput(
+            attrs={
+            'class': 'form-control',
+            'type': 'int'
+            }
+            ),
+            'name': forms.TextInput(
+            attrs={
+            'class': 'form-control'
+            }
+            ),
+            'dn': forms.DateTimeInput(
+            attrs={
+            'class': 'form-control',
+            'type': 'date'
+            }
+            ),
+            'cpf': forms.TextInput(
+            attrs={
+            'class': 'form-control',
+            'type': 'int'
+            }
+            ),
+            'message': forms.Textarea(
+            attrs={
+            'class': 'form-control'
+            }
+            ),
    }
--- app/models.py
+++ app/models.py
@@ -2,14 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Paciente(models.Model):
+    prontuario = models.CharField("Nome", max_length=255, blank = True, null = True)
     name = models.CharField("Nome", max_length=255, blank = True, null = True)
-    responsavel = models.CharField("responsavel", max_length=255, blank = True, null = True)
-    idade = models.DateField('idade', blank = True, null = True)
-    address = models.CharField("Endereço", max_length=255, blank = True, null = True)
-    email = models.EmailField()
+    dn = models.DateField('dn', blank = True, null = True)
+    cpf = models.CharField("cpf", max_length=255, blank = True, null = True)
     message = models.TextField("mensagem", blank=True, null=True)
     createdAt = models.DateTimeField("Criado em", auto_now_add=True)
-    phone = models.CharField('Telefone', max_length=12, blank = True, null = True)
 
     def __str__(self):
         return self.name
