13074dd69336bedcafe5b9bd5ed7dedd27d23d4e
fernandopreviateri <previateri@gmail.com>
2022-12-03 18:12:25 -0300

    Alter Url.py Static

--- univesp/urls.py
+++ univesp/urls.py
@@ -42,7 +42,7 @@ urlpatterns = [
     #path('cadastro/lista/', ListaPaciente.as_view(), name="lista_paciente"),
    
     
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # if settings.DEBUG:
 #     urlpatterns += static(
